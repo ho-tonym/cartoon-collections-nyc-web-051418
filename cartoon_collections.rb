54012caa512e21256feb42f252e19df95fb1c73a
Tony Ho <ho.tonym@gmail.com>
2018-05-07 15:44:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,8 +20,9 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array)
+  array.find do |element|
+    element == "cheddar"
+  end
+
 end
-summon_captain_planet(["apple", "banana", "orange"])
