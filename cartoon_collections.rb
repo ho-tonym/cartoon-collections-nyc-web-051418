4b1da255940c8b287e4db17f8e5acbc2cfd5bf5d
Tony Ho <ho.tonym@gmail.com>
2018-05-07 15:25:55 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  for element in array
+    puts element
+  end
 end
 
 def summon_captain_planet# code an argument here
