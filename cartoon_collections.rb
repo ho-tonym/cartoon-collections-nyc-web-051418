d3cd1c0dd39157ccac08911b607ce8a82ef71da8
Tony Ho <ho.tonym@gmail.com>
2018-05-07 15:40:44 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,12 @@ def summon_captain_planet(array)
   new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  if array.length > 4
+    true
+  elsif array.length < 4
+    false
+  end
 end
 
 def find_the_cheese# code an argument here
